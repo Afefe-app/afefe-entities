6d3082ea56455d2b6c5db1994719970a74fc64da
Sunday Lucky Enyinna <sundayluckyenyinna@gmail.com>
2026-02-27 14:12:32 +0100

    feat: completed start course

--- src/main/java/com/ocean/afefe/entities/modules/enrollments/repository/LessonProgressRepository.java
+++ src/main/java/com/ocean/afefe/entities/modules/enrollments/repository/LessonProgressRepository.java
@@ -4,8 +4,11 @@ import com.ocean.afefe.entities.modules.enrollments.models.LessonProgress;
 import com.ocean.afefe.entities.modules.contents.models.LessonAsset;
 import com.ocean.afefe.entities.modules.enrollments.models.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -13,4 +16,9 @@ import java.util.UUID;
 public interface LessonProgressRepository extends JpaRepository<LessonProgress, UUID> {
 
     Optional<LessonProgress> findByEnrollmentAndLessonAsset(Enrollment enrollment, LessonAsset lessonAsset);
+
+    List<LessonProgress> findByEnrollment(Enrollment enrollment);
+
+    @Query("select lp from LessonProgress lp join fetch lp.enrollment where lp.enrollment.id in :enrollmentIds")
+    List<LessonProgress> findByEnrollmentIdIn(@Param("enrollmentIds") List<UUID> enrollmentIds);
 }
