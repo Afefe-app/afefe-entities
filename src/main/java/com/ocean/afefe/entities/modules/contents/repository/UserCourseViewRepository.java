f755ef93bd821018eb2db5c1373d6433b803ad9b
Sunday Lucky Enyinna <sundayluckyenyinna@gmail.com>
2026-03-26 11:52:05 +0100

    feat: update on entities

--- src/main/java/com/ocean/afefe/entities/modules/contents/repository/UserCourseViewRepository.java
+++ src/main/java/com/ocean/afefe/entities/modules/contents/repository/UserCourseViewRepository.java
@@ -5,11 +5,10 @@ import com.ocean.afefe.entities.modules.contents.models.UserCourseView;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface UserCourseViewRepository extends JpaRepository<UserCourseView, UUID> {
-
-    Optional<UserCourseView> findByUser(User user);
+    List<UserCourseView> findAllByUser(User user);
 }
